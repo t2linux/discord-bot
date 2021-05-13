9bf8c321381064a50c00b99712efd7984dbf6490
networkException <git@nwex.de>
2021-05-14 01:52:27 +0200

    EmojiArgument: Add support for regional indicators
    This is a temporary fix until https://github.com/omnidan/node-emoji/issues/109 gets
    resolved

--- arguments/emojiArgument.ts
+++ arguments/emojiArgument.ts
@@ -7,6 +7,35 @@ export type EmojiType = 'all' | 'unicode' | 'custom';
 
 export class EmojiArgument extends RegexBasedArgument {
 
+    private static readonly regionalIndicators: Map<string, string> = new Map<string, string>([
+        ['🇦', ':regional_indicator_a:'],
+        ['🇧', ':regional_indicator_b:'],
+        ['🇨', ':regional_indicator_c:'],
+        ['🇩', ':regional_indicator_d:'],
+        ['🇪', ':regional_indicator_e:'],
+        ['🇫', ':regional_indicator_f:'],
+        ['🇬', ':regional_indicator_g:'],
+        ['🇭', ':regional_indicator_h:'],
+        ['🇮', ':regional_indicator_i:'],
+        ['🇯', ':regional_indicator_j:'],
+        ['🇰', ':regional_indicator_k:'],
+        ['🇱', ':regional_indicator_l:'],
+        ['🇲', ':regional_indicator_m:'],
+        ['🇳', ':regional_indicator_n:'],
+        ['🇴', ':regional_indicator_o:'],
+        ['🇵', ':regional_indicator_p:'],
+        ['🇶', ':regional_indicator_q:'],
+        ['🇷', ':regional_indicator_r:'],
+        ['🇸', ':regional_indicator_s:'],
+        ['🇹', ':regional_indicator_t:'],
+        ['🇺', ':regional_indicator_u:'],
+        ['🇻', ':regional_indicator_v:'],
+        ['🇼', ':regional_indicator_w:'],
+        ['🇽', ':regional_indicator_x:'],
+        ['🇾', ':regional_indicator_y:'],
+        ['🇿', ':regional_indicator_z:']
+    ]);
+
     public static async parse(message: Message, input: string, type: EmojiType = 'all'): Promise<string | Emoji> {
         if (!input)
             throw CommandError.syntax('EmojiArgument', 'No input provided');
@@ -22,6 +51,13 @@ export class EmojiArgument extends RegexBasedArgument {
             } else {
                 throw CommandError.syntax('EmojiArgument', `Expected unicode emoji but got \`${input.replaceAll('`', '')}\` instead`);
             }
+        }
+
+        if (this.regionalIndicators.has(input)) {
+            if (type === 'custom')
+                throw CommandError.syntax('EmojiArgument', 'Required type custom but input is of type unicode');
+
+            return this.regionalIndicators.get(input);
         }
 
         if (type === 'unicode')
